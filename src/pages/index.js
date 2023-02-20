78b91a8b96c71bea724eb990748157a42a4295a6
Sanyam Jain <sanyamjaino98@gmail.com>
2023-02-20 14:49:38 +0530

    style: theme typography

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import Typography from '@mui/material/Typography'
+import theme from 'src/styles/theme'
 import { makeStyles } from 'tss-react/mui'
 
-const useStyles = makeStyles()((theme, props) => ({
+const useStyles = makeStyles()((props) => ({
   test: {
     display: 'flex',
     justifyContent: 'center',
@@ -12,9 +13,10 @@ const useStyles = makeStyles()((theme, props) => ({
 
 const Home = (props) => {
   const { classes } = useStyles(props)
+  console.log(theme)
 
   return (
-    <Typography variant="h3" className={classes.test}>
+    <Typography variant="h1" className={classes.test}>
       Hello World!
     </Typography>
   )

bdbd1f5265257245148279ad5eff92affe7b01be
Sanyam Jain <sanyamjaino98@gmail.com>
2023-03-10 13:23:51 +0530

    fix: lint and test scripts

---
+++ src/components/card/Card.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+import Card from 'src/components/card/Card'
+
+// @TODO - tests for card module
+test('renders card component', () => {
+  render(<Card />)
+
+  expect(true).toBeTruthy()
+})

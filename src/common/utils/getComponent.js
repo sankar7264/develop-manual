4502069bfbcce94f1325836f633124ae00817ced
Sanyam Jain <sanyam.jain@materialplus.io>
2023-03-28 00:37:30 +0530

    refactor: replaced test logic with quick links module

--- src/common/utils/getComponent.js
+++ src/common/utils/getComponent.js
@@ -1,20 +1,15 @@
-// @TODO - Remove this test component post fixing return statements of getComponent()
-const Test = ({ data }) => <h2>{JSON.stringify(data.section_id)}</h2>
+import QuickLinks from 'src/components/quick-links/QuickLinks'
 
 const MODULES = {
-  SEGMENTED_CONTROl: 'segmented_control',
-  FAQ: 'faq',
   QUICK_LINKS: 'quick_links',
 }
 
 Object.freeze(MODULES)
 
-// @TODO - Replace Test with Correct components
-
 export function getComponent(acf_fc_layout) {
   switch (acf_fc_layout) {
     case MODULES.QUICK_LINKS:
-      return ({ data }) => <Test key={data.section_id} data={data} />
+      return ({ data }) => <QuickLinks key={data.section_id} data={data} />
     default:
       break
   }

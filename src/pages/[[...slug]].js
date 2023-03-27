f04966f3a646278b6b4213f9463a4398a115632b
nakul.kumar <nakul.kumar@materialplus.io>
2023-03-27 12:05:56 +0530

    fix: fixed UI for size smaller than 375px

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -2,7 +2,6 @@ import Typography from '@mui/material/Typography'
 import Layout from 'src/components/layout/Layout'
 import { makeStyles } from 'tss-react/mui'
 import { getHeader, getFooter, getMainNavigation } from 'src/common/services'
-import QuickLinks from 'src/components/quick-links/QuickLinks'
 
 const useStyles = makeStyles()((props) => ({
   test: {
@@ -18,63 +17,21 @@ const Home = (props) => {
   const { headerData, footerData, mainNavigationData } = props
 
   return (
-    <QuickLinks
-      data={{
-        block_section_title: 'Quick Liks',
-        block_section_id: 'quick_link_1',
-        quick_link_title: 'Module Title',
-        quick_link_description:
-          'Cras ac mauris finibus, tempus tellus ut, tempus mi. Vestibulum sed metus aliquet, congue lacus nec, viverra ex. Vivamus non augue vestibulum, sodales felis quis, vestibulum urna.',
-        quick_link_colour: 'cypress_green',
-        quick_link_layout: 'right',
-        quick_links: [
-          {
-            quick_link_link_title: 'Link1',
-            quick_link_url: 'https://presidio.gov/link1',
-            quick_link_link_target: '',
-            quick_link_link_discritpion:
-              'Cras ac mauris finibus, tempus tellus ut, tempus mi.',
-          },
-          {
-            quick_link_link_title: 'Link2',
-            quick_link_url: 'https://presidio.gov/link1',
-            quick_link_link_target: '',
-            quick_link_link_discritpion:
-              'Cras ac mauris finibus, tempus tellus ut, tempus mi.',
-          },
-          {
-            quick_link_link_title: 'Link3',
-            quick_link_url: 'https://presidio.gov/link1',
-            quick_link_link_target: '',
-            quick_link_link_discritpion:
-              'Cras ac mauris finibus, tempus tellus ut, tempus mi.',
-          },
-          {
-            quick_link_link_title: 'Link4',
-            quick_link_url: 'https://presidio.gov/link1',
-            quick_link_link_target: '',
-            quick_link_link_discritpion:
-              'Cras ac mauris finibus, tempus tellus ut, tempus mi.',
-          },
-          {
-            quick_link_link_title: 'Link5',
-            quick_link_url: 'https://presidiotunnel.com/link1',
-            quick_link_link_target: '_blank',
-            quick_link_link_discritpion:
-              'Cras ac mauris finibus, tempus tellus ut, tempus mi.utututCras ac mauris finibus, tempus tellus ut, tempus mi.utututCras ac mauris finibus, tempus tellus ut, tempus mi.utututCras ac mauris finibus, tempus tellus ut, tempus mi.utututCras ac mauris finibus, tempus tellus ut, tempus mi.utututCras ac mauris finibus, tempus tellus ut, tempus mi.ututut',
-          },
-        ],
-      }}
-    />
+    <Layout
+      headerData={headerData}
+      footerData={footerData}
+      mainNavigationData={mainNavigationData}
+    >
+      <Typography variant="h1" className={classes.test}>
+        Hello World!
+      </Typography>
+    </Layout>
   )
 }
 
 export default Home
 
 export async function getServerSideProps(context) {
-  // eslint-disable-next-line global-require
-  require('newrelic')
-
   let headerData = null
   let footerData = null
   let mainNavigationData = null

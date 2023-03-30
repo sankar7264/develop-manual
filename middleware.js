6d2298033778e6b77462d79a89bdd94fc575f6a6
Sanyam Jain <sanyam.jain@materialplus.io>
2023-03-30 17:47:34 +0530

    refactor: reverted the next config

--- middleware.js
+++ middleware.js
@@ -1,8 +1,13 @@
 import { NextResponse } from 'next/server'
 
 export const middleware = (req) => {
+  console.log('health check middleware')
+
   // Required health check endpoint on VIP. Do not remove.
   if (req.nextUrl.pathname === '/cache-healthcheck') {
+    // @TODO - remove logs post testing middleware
+    console.log('health check route matched')
+
     return NextResponse.rewrite(new URL('/api/healthcheck', req.url))
   }
 

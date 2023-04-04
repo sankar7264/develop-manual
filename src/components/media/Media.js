6100c6c9d9cc0777d43715ce4c0df60a122d313c
Shivam Kumar <shivam.kumar@srijan.net>
2023-04-04 16:12:04 +0530

    created utils/css for rem

--- src/components/media/Media.js
+++ src/components/media/Media.js
@@ -5,7 +5,7 @@ import YouTube from 'react-youtube'
 import Image from 'next/image'
 import { Box } from '@mui/material'
 import PropTypes from 'prop-types'
-import rem from 'src/common/utils/calculateRem'
+import rem from 'src/common/utils/css/'
 import useMediaQuery from '@mui/material/useMediaQuery'
 import theme from 'src/styles/theme'
 
